265ce04f2fd77d64560da8b7dac7398478163584
souromisen <71209155+souromisen@users.noreply.github.com>
2021-09-09 19:09:51 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+def madlibs():
+
+	verb=input("Verb: ")
+	adjective=input("Adjective: ")
+	verb1=input("Verb: ")
+	famous_person=input("Famous Person: ")
+	print(f"I love to {verb} because it makes me feel like I'm {adjective}!\nI also enjoy {verb1}ing so that one day I could be as cool as {famous_person}.")
+
+if __name__=='__main__':
+	madlibs()
